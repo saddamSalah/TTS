21d8099a268ed71036a3c7a603237fe02a38648b
saddamSalah <engsadam67@gmail.com>
2018-08-28 14:06:23 +0200

    Delete cleaner.py

--- cleaner.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-meta = pd.read_csv('metadata.csv', delimiter='|')
-meta_df = pd.DataFrame(meta)
-count = 0
-for line in range(len(meta_df.values)):
-    if line == None:
-        count = count + 1
-print(count)
